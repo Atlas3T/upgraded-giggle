8971f83615d985c10aff600a21c539caaae94c26
unknown <alex.white@atlascity.io>
2019-11-11 14:59:13 +0000

    Dispose disposables

---
+++ src/api/DocumentStamp/Helper/CryptoHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Catalyst.Abstractions.Cryptography;
+using Catalyst.Core.Modules.Cryptography.BulletProofs;
+
+namespace DocumentStamp.Helper
+{
+    public static class CryptoHelper
+    {
+        private static readonly Lazy<ICryptoContext> CryptoContext =
+            new Lazy<ICryptoContext>(() => new FfiWrapper());
+
+        public static ICryptoContext GetCryptoContext()
+        {
+            return CryptoContext.Value;
+        }
+    }
+}
