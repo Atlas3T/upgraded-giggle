e2c651e6ecc406be53ec8af52ecaccc376e27dc5
Alex White <alex.white@atlascity.io>
2019-11-15 15:44:21 +0000

    Refactor code

--- src/api/DocumentStamp/Startup.cs
+++ src/api/DocumentStamp/Startup.cs
@@ -1,31 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using Catalyst.Abstractions.IO.Observers;
-using Catalyst.Abstractions.Types;
-using Catalyst.Core.Lib.Cryptography;
 using Catalyst.Core.Lib.Extensions;
-using Catalyst.Core.Lib.IO.EventLoop;
-using Catalyst.Core.Lib.P2P;
-using Catalyst.Core.Lib.Rpc.IO.Messaging.Correlation;
-using Catalyst.Core.Lib.Util;
 using Catalyst.Core.Modules.Cryptography.BulletProofs;
-using Catalyst.Core.Modules.KeySigner;
-using Catalyst.Core.Modules.Rpc.Client;
-using Catalyst.Core.Modules.Rpc.Client.IO.Observers;
-using Catalyst.Core.Modules.Rpc.Client.IO.Transport.Channels;
 using Catalyst.Modules.Repository.CosmosDb;
 using DocumentStamp;
-using DocumentStamp.Keystore;
 using DocumentStamp.Model;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RestSharp;
-using Serilog;
 using TheDotNetLeague.MultiFormats.MultiBase;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -37,13 +19,7 @@ namespace DocumentStamp
         public override void Configure(IFunctionsHostBuilder builder)
         {
             var cryptoContext = new FfiWrapper();
-            var keyStore = new InMemoryKeyStore(cryptoContext,
-                Environment.GetEnvironmentVariable("FunctionPrivateKey"));
-
-            var keyRegistry = new KeyRegistry();
-            var keySigner = new KeySigner(keyStore, cryptoContext, keyRegistry);
-
-            var privateKey = keyRegistry.GetItemFromRegistry(KeyRegistryTypes.DefaultKey);
+            var privateKey = cryptoContext.GetPrivateKeyFromBytes(Environment.GetEnvironmentVariable("FunctionPrivateKey").FromBase32());
             var publicKey = privateKey.GetPublicKey();
             var publicKeyBase32 = publicKey.Bytes.ToBase32();
 
@@ -57,7 +33,7 @@ namespace DocumentStamp
                 recptPublicKey.BuildPeerIdFromBase32Key(recptIp,
                     recptPort);
 
-            var documentStampMetaDataRepository = new CosmosDbRepository<DocumentStampMetaData>("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "mempool", true);
+            var documentStampMetaDataRepository = new CosmosDbRepository<DocumentStampMetaData>(Environment.GetEnvironmentVariable("CosmosDBServer"), Environment.GetEnvironmentVariable("AuthorizationKey"), Environment.GetEnvironmentVariable("DatabaseId"), true);
 
             var restClient = new RestClient(Environment.GetEnvironmentVariable("NodeWebAddress"));
             builder.Services.AddSingleton(restClient);
